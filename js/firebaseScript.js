6165040e5dd2294ba1df14f9b61f9a63ea13fa01
Joshua David Sta. Rita <yehosh.xiii@gmail.com>
2016-04-01 00:01:48 +0800

    Added files via upload

--- js/firebaseScript.js
+++ js/firebaseScript.js
@@ -1,6 +1,24 @@
-const db = new Firebase('https://eventguestatttendance.firebaseio.com/');
-    const submitBtn = $('#submit');
-    const nameField = $('#name');
-    const orgField = $('#organization');
-    const contactField = $('#contact_number');
-    const items = $('#items');
+var fbdbReference = new Firebase('https://eventguestlisting.firebaseio.com/');
+function addGuest(){
+	var name = $('#name').val();
+	var organization = $('#org').val();
+	var contactNumber = $('#contact').val();
+
+	fbdbReference.push({name: name, organization: organization, contactNumber: contactNumber});
+	
+	fbdbReference.on('child_added', function(snapshot) {
+    	var message = snapshot.val();
+    	newGuestPopup(message.name, message.organization, message.contactNumber);
+    });
+}
+    function appendGuest(name, organization, contactNumber) {
+      	const newGuest = 
+      	'<li><h4>' + name + '</h4>' +
+      	'<h5>' + organization + '</h5>' +
+      	'<i class = "contact">' + contactNumber + '</i></li>';
+      	$('#guests').append(newGuest);
+	}
+	function newGuestPopup((name, organization, contactNumber) {
+      	alert(
+      	'Name:' + name + 'Organization: ' + organization + 'Contact Number:' + contactNumber;
+    }
